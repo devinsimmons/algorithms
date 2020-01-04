2a37a92d98c5558ad85ed7baa94365fae122a361
devinsimmons <devinsimmons71@gmail.com>
2020-01-04 15:01:14 -0500

    made it faster
    made mergesort faster by merging items into empty array rather than an array of size n filled with zeros

--- merge_sort/visualize_mergesort.py
+++ merge_sort/visualize_mergesort.py
@@ -5,7 +5,7 @@ class MergeSort:
     def __init__(self, arr: list):
         self.arr = arr
     
-    def sort(self, how = 'low_to_high'):
+    def sort1(self, how = 'low_to_high'):
         if how == 'low_to_high':
             return MergeSort.order(self.arr)
     
@@ -15,6 +15,8 @@ class MergeSort:
         #array of length 1
         if mid < 2:
             return arr
+        
+        
         #arrs length 2
         elif len(arr) == 2:
             return MergeSort.merge([arr[0]], [arr[1]])
@@ -27,42 +29,47 @@ class MergeSort:
     #merges two sorted arrays
     @staticmethod
     def merge(arr1, arr2):
-        new_arr = [0] * len(arr1) + [0] * len(arr2)
-        
+        #faster to start with empty list than a list of length n
+        new_arr = []
         #index of the first arr  
         i = 0
         #index of the second arr
         j = 0
-        #index of the final arr
-        k  = 0
         
         #do quick merge, compare entry from left list to right list, put lower 
         ##value in the new arr
         for x in range(0, len(new_arr)):
 
             if arr1[i] < arr2[j]:
-                new_arr[k] = arr1[i]
+                new_arr.append(arr1[i])
                 if (i+1) < len(arr1):
                     i += 1
+                    
                 #add remaining list to the end
                 else:
-                    new_arr[k+1:] = arr2[j:]
+                    new_arr += arr2[j:]
                     break
             else:
-                new_arr[k] = arr2[j]
+                new_arr.append(arr2[j])
                 if (j+1) < len(arr2):
                     j += 1
                 else:
-                    new_arr[k+1:] = arr1[i:]
+                    new_arr += arr1[i:]
                     break
-            k += 1
         return new_arr
 
 import random
 import time
-lis =[random.randrange(1, 100) for i in range(0, 300000)]
+lis =[random.randrange(1, 100000000) for i in range(0, 1000000)]
+test = MergeSort(lis)
 
 st = time.time()
-test = MergeSort(lis)
-test.sort()
+
+test.sort1()
 print(time.time() - st)
+
+st = time.time()
+
+lis.sort()
+print(time.time() - st)
+
