b043c55e865ef3c2aade95bc763409f51188f0a4
devin <devinsimmons71@gmail.com>
2020-01-07 09:37:58 -0500

    improved inheritance to reduce reptitive code
    instead of modifying merge function with each inherited class, I created a comparator function that is called by then merge function

--- merge_sort/mergesort.py
+++ merge_sort/mergesort.py
@@ -31,9 +31,12 @@ class MergeSort:
             arr2 = self.order(arr[mid:])
             return self.merge(arr1, arr2)            
     
-
+    #this function compares two values to determine insertion order
+    def comparator(self, obj1, obj2):
+        return obj1 < obj2
+    
     #merges two sorted arrays
-    def merge(self, arr1, arr2, op = operator.lt):
+    def merge(self, arr1, arr2):
         #faster to start with empty list than a list of length n
         new_arr = []
         #index of the first arr  
@@ -45,7 +48,7 @@ class MergeSort:
         ##value in the new arr
         for x in range(0, (len(arr1) + len(arr2))):
             #essentially says if arr1[i] < arr2[j]
-            if op(arr1[i], arr2[j]):
+            if self.comparator(arr1[i], arr2[j]):
                 new_arr.append(arr1[i])
                 if (i+1) < len(arr1):
                     i += 1
@@ -63,15 +66,16 @@ class MergeSort:
                     break
         return new_arr
     
+    
+            
+    
 
 #class that orders the list from high to low. 
 class HighToLow(MergeSort):
     
-    #merges two sorted arrays, ordering from high to low
-    #the operator is switched to be >
-    def merge(self, arr1, arr2):
-        #changes the default arg for op
-        return super().merge(arr1, arr2, op = operator.gt)
+    #changes basis for comparison
+    def comparator(self, obj1, obj2):
+        return obj1 > obj2
 
 #class that applies mergesort to points, ordering them by the slope they have
 #to another point
@@ -87,39 +91,13 @@ class MergeSortSlopes(MergeSort):
         else:
             self.pt = pt
     
-    #modifying merge function so that it makes comparison based on slope from 
-    #points in array to the target point object
-    def merge(self, arr1, arr2, op = operator.lt):
-        #faster to start with empty list than a list of length n
-        new_arr = []
-        #index of the first arr  
-        i = 0
-        #index of the second arr
-        j = 0
+    #comparison is now made between slopes
+    def comparator(self, obj1, obj2):
+        slope1 = self.pt.calcSlope(Point(obj1))
+        slope2 = self.pt.calcSlope(Point(obj2))
         
-        #do quick merge, compare entry from left list to right list, put lower 
-        ##value in the new arr
-        for x in range(0, (len(arr1) + len(arr2))):
-            slope1 = self.pt.calcSlope(Point(arr1[i]))
-            slope2 = self.pt.calcSlope(Point(arr2[j]))
-            
-            if op(slope1, slope2):
-                new_arr.append(arr1[i])
-                if (i+1) < len(arr1):
-                    i += 1
-                    
-                #add remaining list to the end
-                else:
-                    new_arr += arr2[j:]
-                    break
-            else:
-                new_arr.append(arr2[j])
-                if (j+1) < len(arr2):
-                    j += 1
-                else:
-                    new_arr += arr1[i:]
-                    break
-        return new_arr
+        return slope1 < slope2
+    
 #point class (x and y coordinates)   
 class Point:
     
@@ -148,5 +126,4 @@ for i in points:
         if x != (len(pts) - 1):
             if pt.calcSlope(Point(pts[x])) == pt.calcSlope(Point(pts[x+1])):
                 print(pts[x], pts[x+1])        
-            
-        
+                   
