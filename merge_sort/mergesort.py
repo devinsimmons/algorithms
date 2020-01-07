385a5ee06e32e0df2826a5e3bfc850762439a5a0
devinsimmons <devinsimmons71@gmail.com>
2020-01-06 23:21:00 -0500

    can now order points by slope to another respective point

--- merge_sort/mergesort.py
+++ merge_sort/mergesort.py
@@ -73,15 +73,80 @@ class HighToLow(MergeSort):
         #changes the default arg for op
         return super().merge(arr1, arr2, op = operator.gt)
 
-#point class    
+#class that applies mergesort to points, ordering them by the slope they have
+#to another point
+class MergeSortSlopes(MergeSort):
+    
+    def __init__(self, arr: list, pt):
+        self.arr = arr
+        #create point object that is the basis of all comparisons made in the 
+        #modified merge function
+        #can take either a tuple of coords or a pt object
+        if type(pt) == tuple:
+            self.pt = Point(pt)
+        else:
+            self.pt = pt
+    
+    #modifying merge function so that it makes comparison based on slope from 
+    #points in array to the target point object
+    def merge(self, arr1, arr2, op = operator.lt):
+        #faster to start with empty list than a list of length n
+        new_arr = []
+        #index of the first arr  
+        i = 0
+        #index of the second arr
+        j = 0
+        
+        #do quick merge, compare entry from left list to right list, put lower 
+        ##value in the new arr
+        for x in range(0, (len(arr1) + len(arr2))):
+            slope1 = self.pt.calcSlope(Point(arr1[i]))
+            slope2 = self.pt.calcSlope(Point(arr2[j]))
+            
+            if op(slope1, slope2):
+                new_arr.append(arr1[i])
+                if (i+1) < len(arr1):
+                    i += 1
+                    
+                #add remaining list to the end
+                else:
+                    new_arr += arr2[j:]
+                    break
+            else:
+                new_arr.append(arr2[j])
+                if (j+1) < len(arr2):
+                    j += 1
+                else:
+                    new_arr += arr1[i:]
+                    break
+        return new_arr
+#point class (x and y coordinates)   
 class Point:
     
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, coords: tuple):
+        self.x = coords[0]
+        self.y = coords[1]
     
     #point should be an instance of the point class
     def calcSlope(self, point):
+        #handle undefined slope
+        if self.y == point.y and self.x != point.x:
+            return float('inf')
+        #handle situation where both points are equal
+        elif self.y == point.y and self.x == point.x:
+            return -float('inf')
+        #normal slope equation
         return (self.y - point.y)/(self.x - point.x)
     
+points = [(10, 9), (9, 8), (8, 7), (1, 1)]
+
+for i in points:
+    pt = Point(i)
+    pts = MergeSortSlopes(points, pt).sort()
     
+    for x in range(0, len(pts)):
+        if x != (len(pts) - 1):
+            if pt.calcSlope(Point(pts[x])) == pt.calcSlope(Point(pts[x+1])):
+                print(pts[x], pts[x+1])        
+            
+        
