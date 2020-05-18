00a81dd9ff3edfbf89b9c0fa7ace9fa8d434de7e
devinsimmons <devinsimmons71@gmail.com>
2020-05-18 01:02:50 -0400

    created a working script to generate geometry for the shortest path

--- routing_project/scripts/dijkstra_dc.py
+++ routing_project/scripts/dijkstra_dc.py
@@ -1,4 +1,5 @@
 import fiona
+from shapely.geometry import shape, LineString
 import os
 import geopandas as gpd
 
@@ -9,62 +10,84 @@ class Graph:
     
     #the version I am currently developing requires that the infile is a GPKG
     #layer_name is the name of the target layer in the GPKG
-    def __init__(self, infile: str, layer_name: str):
+    def __init__(self, infile: str, layer_name: str, node_layer: str):
         #get number of nodes so I can start building the adjacency list
-        num_nodes = len(fiona.open(infile, layer = 'road_nodes'))
-        #the node ids start at 1
-        self.nodes = [LinkedList(i + 1) for i in range(0, num_nodes)]
+        num_nodes = len(fiona.open(infile, layer = node_layer))
+        #this dictionary will contain adjacency lists for each node in the network
+        self.nodes = {}
         
-        self.adj_list = []
-        
+        #build the dictionary containing node attributes
+        with fiona.open(infile, layer = node_layer) as layer:
+            for feature in layer:
+                node_id = list(feature['properties'].items())[0][1]
+                self.nodes[node_id] = LinkedList(node_id, None, shape(feature['geometry']))
+                
         with fiona.open(infile, layer = layer_name) as layer:
             for feature in layer:
                 src = feature['properties']['source']
                 tgt = feature['properties']['target']
                 cost = feature['properties']['cost']
-                geom = feature['geometry']
-                #adds connection to the adjacency list. i am subtracting one bc
-                #the nodes index from one
-                self.nodes[src - 1].insert(LinkedList(tgt, cost, geom))
+                geom = shape(feature['geometry'])
+                
+                #adds connection to the adjacency list.
+                self.nodes[src].insert(LinkedList(tgt, cost, geom))
                 
                 #adds the reverse connection from target to source if it is not
                 #a one way street
                 if feature['properties']['reverse_cost'] == cost:
-                    self.nodes[tgt - 1].insert(LinkedList(src, cost, geom))
+                    #reverse the geometry of the linestring, makes it easier
+                    #down the line to construct the shortest path
+                    #absolutely bizzare but I can't reverse this geom while referencing
+                    #the original geom. it fucks up the geom value that has already 
+                    #been stored in a linked list. very confused on this
+                    reverse_geom = shape(feature['geometry'])
+                    reverse_geom.coords = list(reverse_geom.coords[::-1])
                 
-    #node1 is the integer that represents the start node's gid, node2 is the end node
-    def dijkstra(self, node1: int, node2: int):
+                    self.nodes[tgt].insert(LinkedList(src, cost, reverse_geom))
+                    
+
+    #node1 is the integer that represents the start node's gid
+    #it returns a table that contains all the nodes' distance to the starting node,
+    #in addition to attributes that allow the user to retrace the path to the 
+    #starting node
+    def dijkstra(self, node1: int):
         #i am storing data on the nodes in a variety of dictionaries. the key is 
         #the node gid and the value is the relevant value (distance, visited, prev_node)
         
-        self.df = gpd.GeoDataFrame({'gid': [i.gid for i in self.nodes], 
+        self.df = gpd.GeoDataFrame({'gid': [self.nodes[i].gid for i in self.nodes], 
                                  'distance': [float('inf') for i in self.nodes],
-                                 'prev_node': [i.gid for i in self.nodes],
-                                 'unvisited': [True for i in self.nodes]})
-        self.df.index += 1
+                                 'prev_node': [self.nodes[i].gid for i in self.nodes],
+                                 'unvisited': [True for i in self.nodes],
+                                  'geom': [self.nodes[i].geom for i in self.nodes]}).set_geometry('geom')
+        
+    
+   
         #distance from node1 to node1 is 0
         self.df.loc[self.df['gid'] == node1, ['distance']] = 0
         
         #getting the actual objects that represent the nodes
-        self.node1 = self.nodes[node1 - 1]
-        node2 = self.nodes[node2 - 1]
-        
-        self.counter = 0
+        self.node1 = self.nodes[node1]
+
         while len(self.df[self.df['unvisited'] == True]) > 0:
             self.visitNeighbors()
+        
+        self.df = self.df[self.df['distance'] < float('inf')]
+        return self.df
 
         
     #iterate through a node's neighbors, determine their distance to the starting node
     #node is a LinkedList object
     def visitNeighbors(self):
         #find the smallest value in the dataframe 
-        node = self.nodes[self.df[self.df['unvisited'] == True]['distance'].idxmin() - 1]
+        min_index = self.df[self.df['unvisited'] == True]['distance'].idxmin()
+
+        node = self.nodes[self.df.iloc[min_index].gid]
         tgt_node = node
         
         while tgt_node.next:
             
             neighbor = tgt_node.next
-            new_distance = neighbor.cost + self.df[self.df['gid'] == tgt_node.gid]['distance'].values[0]
+            new_distance = neighbor.cost + self.df[self.df['gid'] == node.gid]['distance'].values[0]
 
             #filter for values that are unvisited, equal the gid we want, and have a higher distance than the new distance
             df_gid = self.df[self.df['gid'] == neighbor.gid]
@@ -73,15 +96,17 @@ class Graph:
             
             #this part needs some work. time for bed
             if len(df_dist) > 0:
-                
-                self.df.loc[neighbor.gid, ['distance']] = new_distance
-                self.df.loc[neighbor.gid, ['prev_node']] = tgt_node.gid
-                
+                self.df.loc[self.df['gid'] == neighbor.gid, ['distance']] = new_distance
+                self.df.loc[self.df['gid'] == neighbor.gid, ['prev_node']] = node.gid
+                #setting neighbor geometry equal to geometry from the node to its neighbor 
+                #these will later be concatenated when the shortest path is determined
+                #having trouble here
+                self.df.loc[self.df['gid'] == neighbor.gid, ['geom']] = neighbor.geom
+                                
             #move on to the next neighbor
             tgt_node = neighbor
         #node has been visited
-        self.df.loc[node.gid, ['unvisited']] = False
-        print(node.gid)
+        self.df.loc[self.df['gid'] == node.gid, ['unvisited']] = False
 
 
     
@@ -98,6 +123,39 @@ class LinkedList:
             self.next = node
         else:
             self.next.insert(node)
+
+#df is the return value of the Graph.dijkstra function
+#tgt is the gid of the node that you want to reach
+def shortestPath(df: Graph.dijkstra, tgt: int):
+    #list that will contain linestring geometries that must be concatenated together
+    #this will be treated as a stack, in that the last geom added will be the
+    #first to be added to the final line segment 
+    geoms = []
+    coords = []
+    
+    src = df[df['distance'] == 0]['gid'].values[0]
+
+    while tgt != src:
+        geoms.append(df[df['gid'] == tgt]['geom'].values[0])
+        tgt = df[df['gid'] == tgt]['prev_node'].values[0]
+    
+    while len(geoms) > 0:
+        seg = geoms.pop()
+        seg_coords = list(seg.coords)
+
+        coords += seg_coords
+
+    path = LineString(coords)
+    return path
+
+            #56269
    
-graph = Graph('routing_project.gpkg', 'dc_roads')
-graph.dijkstra(10, 11)
+    
+   
+graph = Graph('test_data.gpkg', 'dc_roads', 'road_nodes')
+paths = graph.dijkstra(60288)
+
+
+gdf = gpd.GeoDataFrame({'gid': [graph.nodes[i].gid for i in paths['gid'].values if graph.nodes[i].gid != 292092],
+                        'geom': [shortestPath(paths, graph.nodes[i].gid) for i in paths['gid'].values   if graph.nodes[i].gid != 292092]}).set_geometry('geom')
+gdf.to_file('dijkstra.shp')
